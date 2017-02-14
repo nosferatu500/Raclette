6405b13fede4df33940296d8ed3271dc73bd8dea
vitalya199529 <vitalya199529@mail.ru>
2017-02-14 19:06:49 +0700

    Add project files.

---
+++ WebApplicationTest/Checkout/CheckoutStart.aspx.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplicationTest.Checkout
+{
+    public partial class CheckoutStart : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            NVPAPICaller payPalCaller = new NVPAPICaller();
+            string retMsg = "";
+            string token = "";
+
+            if (Session["payment_amt"] != null)
+            {
+                //string amt = Session["payment_amt"].ToString();
+                var temp = Math.Round(Convert.ToDecimal(Session["payment_amt"]), 2).ToString();
+                var replace = temp.Replace(",", ".");
+                string amt = replace;
+
+                bool ret = payPalCaller.ShortcutExpressCheckout(amt, ref token, ref retMsg);
+                if (ret)
+                {
+                    Session["token"] = token;
+                    Response.Redirect(retMsg);
+                }
+                else
+                {
+                    Response.Redirect("CheckoutError.aspx?" + retMsg);
+                }
+            }
+            else
+            {
+                Response.Redirect("CheckoutError.aspx?ErrorCode=AmtMissing");
+            }
+        }
+    }
+}
---
+++ WebApplicationTest/Models/Product.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationTest.Models
+{
+    public class Product
+    {
+        [ScaffoldColumn(false)]
+        public int ProductID { get; set; }
+
+        [Required, StringLength(100), Display(Name = "Name")]
+        public string ProductName { get; set; }
+
+        [Required, StringLength(10000), Display(Name = "Product Description"), DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        public string ImagePath { get; set; }
+
+        [Display(Name = "Price")]
+        public double? UnitPrice { get; set; }
+
+        public long MinorValueAmount
+        {
+            get
+            {
+                return (long)Math.Round(UnitPrice.Value * 100);
+            }
+        }
+        public string UtcValueAmount
+        {
+            get
+            {
+                var minor = MinorValueAmount;
+                var fractional = string.Empty;
+                if (10 > minor % 100)
+                {
+                    fractional = $"0{minor % 100}";
+                }
+                else
+                {
+                    fractional = $"{minor % 100}";
+                }
+
+                return $"{minor / 100}.{fractional}";
+            }
+        }
+
+        public int? CategoryID { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
