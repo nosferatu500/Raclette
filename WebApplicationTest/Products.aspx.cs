cf9cbb4195f5eaccc5c2436836098d70e8be1df5
vitalya199529 <vitalya199529@mail.ru>
2017-02-16 01:39:59 +0700

    Fix Stile for ProductList

--- WebApplicationTest/Products.aspx.cs
+++ WebApplicationTest/Products.aspx.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.ModelBinding;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using WebApplicationTest.Models;
 
 namespace WebApplicationTest
 {
@@ -12,6 +14,23 @@ namespace WebApplicationTest
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        public IQueryable<Product> GetListProducts([QueryString("id")] int? categoryId, [RouteData] string categoryName)
+        {
+            var _db = new ProductContext();
+            IQueryable<Product> query = _db.Products;
+
+            if (categoryId.HasValue && categoryId > 0)
+            {
+                query = query.Where(p => p.CategoryID == categoryId);
+            }
+
+            if (!String.IsNullOrEmpty(categoryName))
+            {
+                query = query.Where(p => String.Compare(p.Category.CategoryName, categoryName) == 0);
+            }
+            return query;
         }
     }
 }
