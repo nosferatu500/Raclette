d3aaea7d9d8609bfaae72747c577e84c0a9bd409
vitalya199529 <vitalya199529@mail.ru>
2017-02-15 12:24:35 +0700

    Added Categories

---
+++ WebApplicationTest/Models/Categor/Food.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationTest.Models.Categor
+{
+    public enum Food
+    {
+        Breakfast = 1,
+        Lunch = 2,
+        Dinner = 3
+    }
+}
