f93de0217cca502a13959313f55d35e208bbaa8a
vitalya199529 <vitalya199529@mail.ru>
2017-02-17 00:31:00 +0700

    Set Web.config and Models

---
+++ NewWebApplication/Models/OrderDetail.cs
@@ -0,0 +1,17 @@
+﻿namespace NewWebApplication.Models
+{
+    public class OrderDetail
+    {
+        public int OrderDetailId { get; set; }
+
+        public int OrderId { get; set; }
+
+        public string Username { get; set; }
+
+        public int ProductId { get; set; }
+
+        public int Quantity { get; set; }
+
+        public double? UnitPrice { get; set; }
+    }
+}
---
+++ NewWebApplication/Models/Product.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace NewWebApplication.Models
+{
+    public class Product
+    {
+        [ScaffoldColumn(false)]
+        public int ProductID { get; set; }
+
+        [Required, StringLength(100), Display(Name = "Name")]
+        public string ProductName { get; set; }
+
+        [Required, StringLength(10000), Display(Name = "Product Description"), DataType(DataType.MultilineText)]
+        public string Description { get; set; }
+
+        public string ImagePath { get; set; }
+
+        [Display(Name = "Price")]
+        public double? UnitPrice { get; set; }
+
+        public double? Discount => UnitPrice.Value - UnitPrice.Value / 10;
+
+        public long MinorValueAmount
+        {
+            get
+            {
+                return (long)Math.Round(UnitPrice.Value * 100);
+            }
+        }
+        public string UtcValueAmount
+        {
+            get
+            {
+                var minor = MinorValueAmount;
+                var fractional = string.Empty;
+                if (10 > minor % 100)
+                {
+                    fractional = $"0{minor % 100}";
+                }
+                else
+                {
+                    fractional = $"{minor % 100}";
+                }
+
+                return $"{minor / 100}.{fractional}";
+            }
+        }
+
+        public int? CategoryID { get; set; }
+
+        public virtual Category Category { get; set; }
+    }
+}
