008a9945cddeb90e09a7a2078453980ca24a0692
vitalya199529 <vitalya199529@mail.ru>
2017-02-17 02:30:52 +0700

    Pre-Complete move data from Site.Master to _Layout

--- NewWebApplication/Controllers/HomeController.cs
+++ NewWebApplication/Controllers/HomeController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using NewWebApplication.Models;
+using WebApplicationTest.Logic;
 
 namespace NewWebApplication.Controllers
 {
@@ -25,6 +27,30 @@ namespace NewWebApplication.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (System.Web.HttpContext.Current.User.IsInRole("canEdit"))
+            {
+                adminLink.Visible = true;
+            }
+        }
+
+        protected void Page_PreRender(object sender, EventArgs e)
+        {
+            using (ShoppingCartActions usersShoppingCart = new ShoppingCartActions())
+            {
+                string cartStr = string.Format("Cart ({0})", usersShoppingCart.GetCount());
+                cartCount.InnerText = cartStr;
+            }
+        }
+
+        public IQueryable<Category> GetCategories()
+        {
+            var _db = new ProductContext();
+            IQueryable<Category> query = _db.Categories;
+            return query;
         }
     }
 }
